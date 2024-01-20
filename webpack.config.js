81356a7c29339e47a7c8cd30571cac0886c9af02
webshunter <webshunternet@gmail.com>
2024-01-20 13:10:11 +0700

    upload

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+  "mode": "none",
+  "entry": "./src/scripts.js",
+  "output": {
+    "path": __dirname + '/dist',
+    "filename": "bundle.js"
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist')
+    }
+  },
+  "devtool": "source-map",
+  "module": {
+    "rules": [
+      {
+        test: /\.css$/,
+        use: [
+            { loader: 'style-loader' },
+            { loader: 'css-loader' }
+        ]
+    },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/',
+              publicPath: 'images/'
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
