8785d686a5454bee54b3e0d1625bc5bd44ca3362
webshunternet <webshunternet@gmail.com>
2024-01-20 15:55:41 +0700

    update

--- src/scripts.js
+++ src/scripts.js
@@ -5,7 +5,11 @@
 import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import './images/turing-logo.png';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
+import ezuikit from 'ezuikit-js';
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
