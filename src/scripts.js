5519306e9efb95cb245c586d3d027dc60ce3186c
webshunter <webshunternet@gmail.com>
2024-01-22 13:18:57 +0700

    update tampilan

--- src/scripts.js
+++ src/scripts.js
@@ -1,24 +1,25 @@
 
-import './css/styles.css';
-
-import './images/turing-logo.png';
-
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 import ezuikit from 'ezuikit-js';
 
 console.log(ezuikit);
 
+var player1 = document.getElementById("video-container");
+
+var w = player1.clientWidth;
+
 var player = new ezuikit.EZUIKitPlayer({
     id: 'video-container',
     url: 'ezopen://open.ezviz.com/D76265573/1.live',
     autoplay: true,
     accessToken: "at.c9rdnxkgdfms7jdl4p155a974o3iirs7-2veg476opa-1vbju1s-dg739pgxc",
-    width: 600,
+    width: w,
     height: 400,
     env:{
     	domain: "https://isgpopen.ezvizlife.com"
     }
 });
+
 
 player.getRealUrl()
