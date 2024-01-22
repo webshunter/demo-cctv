267844c25ffbb8c93111c833a91252744eced23e
webshunter <webshunternet@gmail.com>
2024-01-22 17:01:41 +0700

    Updates

--- bundle.js
+++ bundle.js
@@ -578,11 +578,11 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-console.log((ezuikit_js__WEBPACK_IMPORTED_MODULE_1___default()));
-
 var player1 = document.getElementById("video-container");
+var player2 = document.getElementById("video-container2");
 
 var w = player1.clientWidth;
+var w2 = player2.clientWidth;
 
 var player = new (ezuikit_js__WEBPACK_IMPORTED_MODULE_1___default().EZUIKitPlayer)({
     id: 'video-container',
@@ -596,17 +596,23 @@ var player = new (ezuikit_js__WEBPACK_IMPORTED_MODULE_1___default().EZUIKitPlaye
     	domain: "https://isgpopen.ezvizlife.com"
     }
 });
-document.getElementById('video-container-play-content').setAttribute("title","Play/Stop");
-document.getElementById('video-container-capturePicture-content').setAttribute("title","Screenshot");
-document.getElementById('video-container-sound-content').setAttribute("title","Sound");
-document.getElementById('video-container-pantile-content').setAttribute("title","PTZ Control");
-document.getElementById('video-container-recordvideo-content').setAttribute("title","Record Screen");
-document.getElementById('video-container-talk-content').setAttribute("title","Talk");
-document.getElementById('video-container-zoom-content').setAttribute("title","Zoommmm");
-document.getElementById('video-container-hd-content').setAttribute("title","Video Resolution");
-document.getElementById('video-container-webExpend-content').setAttribute("title","Full Screen Mode");
-document.getElementById('video-container-expend-content').setAttribute("title","Full Screen");
-player.getRealUrl();
+
+var player2 = new (ezuikit_js__WEBPACK_IMPORTED_MODULE_1___default().EZUIKitPlayer)({
+    id: 'video-container2',
+    url: 'ezopen://open.ezviz.com/D78805686/1.live',
+    autoplay: true,
+    accessToken: "at.8b307h0d69oq3i4s1sfdtssy02d05m2v-6t6tl7w1b7-1msu7lt-nwd7uc2k5",
+    template: 'pcLive',
+    width: w,
+    height: 400,
+    env:{
+    	domain: "https://isgpopen.ezvizlife.com"
+    }
+});
+
+
+player.getRealUrl()
+
 })();
 
 /******/ })()
