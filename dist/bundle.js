3bc19acc5563d24d5e28047166e929f62912bf64
webshunter <webshunternet@gmail.com>
2024-01-22 21:59:09 +0700

    update

--- dist/bundle.js
+++ dist/bundle.js
@@ -578,40 +578,133 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-var player1 = document.getElementById("video-container");
-var player2 = document.getElementById("video-container2");
 
-var w = player1.clientWidth;
-var w2 = player2.clientWidth;
 
-var player = new (ezuikit_js__WEBPACK_IMPORTED_MODULE_1___default().EZUIKitPlayer)({
-    id: 'video-container',
-    url: 'ezopen://open.ezviz.com/D76265573/1.live',
-    autoplay: true,
-    accessToken: "at.c9rdnxkgdfms7jdl4p155a974o3iirs7-2veg476opa-1vbju1s-dg739pgxc",
-    template: 'pcLive',
-    width: w,
-    height: 400,
-    env:{
-    	domain: "https://isgpopen.ezvizlife.com"
+function POST(ln) {
+    this.link = ln;
+    this.dataFD = new FormData();
+
+    this.data = function (a) {
+        let q = this;
+        if (typeof a === 'object') {
+            let param = Object.keys(a);
+            param.forEach(function (o) {
+                q.dataFD.append(o, a[o]);
+            });
+        }
+        return q;
+    };
+
+    this.then = function (callback, funcerr) {
+        fetch(this.link, {
+            body: this.dataFD,
+            method: "POST", // Gunakan huruf besar untuk metode HTTP
+        })
+            .then(function (r) {
+                return r.json();
+            })
+            .then(callback)
+            .catch(funcerr);
+    };
+
+    return this;
+}
+
+const plyr = {}
+
+plyr.player2 = function(token){
+    let player = document.getElementById("video-container2");
+    let w2 = player.clientWidth;
+    const ply = new (ezuikit_js__WEBPACK_IMPORTED_MODULE_1___default().EZUIKitPlayer)({
+        id: 'video-container2',
+        url: 'ezopen://open.ezviz.com/D78805686/1.live',
+        autoplay: true,
+        accessToken: token,
+        template: 'pcLive',
+        width: w2,
+        height: 400,
+        env: {
+            domain: "https://isgpopen.ezvizlife.com"
+        }
+    });
+    return ply;
+} 
+
+plyr.player = function (token){
+    var player1 = document.getElementById("video-container");
+    var w = player1.clientWidth;
+    const ply = new (ezuikit_js__WEBPACK_IMPORTED_MODULE_1___default().EZUIKitPlayer)({
+        id: 'video-container',
+        url: 'ezopen://open.ezviz.com/D76265573/1.live',
+        autoplay: true,
+        accessToken: token,
+        template: 'pcLive',
+        width: w,
+        height: 400,
+        env: {
+            domain: "https://isgpopen.ezvizlife.com"
+        }
+    });
+} 
+
+const callNewToken = function(id , idplay, appKey, appSecret){
+    new POST('https://open.ezvizlife.com/api/lapp/token/get')
+        .data({
+            appKey: appKey, appSecret: appSecret
+        })
+        .then(function (res) {
+            let data = {
+                token: res.data.accessToken ? res.data.accessToken : null,
+                expired: res.data.expireTime ? res.data.expireTime : null
+            };
+            localStorage.setItem(id, btoa(JSON.stringify(data)));
+            plyr[idplay](data.token);
+        }, function (err) {
+            console.log(err)
+        })
+}
+
+if(!localStorage.getItem('key-client')){
+    callNewToken('key-client', 'player', 'ba8856216e3b4f2681c0c7c706d37df8', '50793f889ca74387bb9028029da6137b');
+} else {
+    let data = JSON.parse(atob(localStorage.getItem('key-client')));
+    if (data.expired < Date.now()){
+        callNewToken('key-client', 'player', 'ba8856216e3b4f2681c0c7c706d37df8', '50793f889ca74387bb9028029da6137b');
+    }else{
+        plyr['player'](data.token)
+        console.log(data.expired)
     }
-});
+};
 
-var player2 = new (ezuikit_js__WEBPACK_IMPORTED_MODULE_1___default().EZUIKitPlayer)({
-    id: 'video-container2',
-    url: 'ezopen://open.ezviz.com/D78805686/1.live',
-    autoplay: true,
-    accessToken: "at.8b307h0d69oq3i4s1sfdtssy02d05m2v-6t6tl7w1b7-1msu7lt-nwd7uc2k5",
-    template: 'pcLive',
-    width: w,
-    height: 400,
-    env:{
-    	domain: "https://isgpopen.ezvizlife.com"
+if(!localStorage.getItem('key-client2')){
+    callNewToken('key-client2', 'player2', 'cd3807c3ddf64098a4628eb188101b04', '50793f889ca74387bb9028029da6137b');
+} else {
+    let data = JSON.parse(atob(localStorage.getItem('key-client2')));
+    if (data.expired < Date.now()){
+        callNewToken('key-client2', 'player2', 'cd3807c3ddf64098a4628eb188101b04', '50793f889ca74387bb9028029da6137b');
+    }else{
+        plyr['player2'](data.token)
+        console.log(data.expired)
     }
-});
+}
 
 
-player.getRealUrl()
+
+
+
+
+
+document.getElementById('video-container-play-content').setAttribute("title","Play/Stop");
+document.getElementById('video-container-capturePicture-content').setAttribute("title","Screenshot");
+document.getElementById('video-container-sound-content').setAttribute("title","Sound");
+document.getElementById('video-container-pantile-content').setAttribute("title","PTZ Control");
+document.getElementById('video-container-recordvideo-content').setAttribute("title","Record Screen");
+document.getElementById('video-container-talk-content').setAttribute("title","Talk");
+document.getElementById('video-container-zoom-content').setAttribute("title","Zoom");
+document.getElementById('video-container-hd-content').setAttribute("title","Video Resolution");
+document.getElementById('video-container-webExpend-content').setAttribute("title","Full Screen Mode");
+document.getElementById('video-container-expend-content').setAttribute("title","Full Screen");
+player.getRealUrl();
 
 })();
 
--- src/scripts.js
+++ src/scripts.js
@@ -3,38 +3,121 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 import ezuikit from 'ezuikit-js';
 
-var player1 = document.getElementById("video-container");
-var player2 = document.getElementById("video-container2");
 
-var w = player1.clientWidth;
-var w2 = player2.clientWidth;
 
-var player = new ezuikit.EZUIKitPlayer({
-    id: 'video-container',
-    url: 'ezopen://open.ezviz.com/D76265573/1.live',
-    autoplay: true,
-    accessToken: "at.2woo7ro34v66nia077hwk23n3fa1rvbf-3k7pom88cq-19muc9b-oppzktz1e",
-    template: 'pcLive',
-    width: w,
-    height: 400,
-    env:{
-    	domain: "https://isgpopen.ezvizlife.com"
+function POST(ln) {
+    this.link = ln;
+    this.dataFD = new FormData();
+
+    this.data = function (a) {
+        let q = this;
+        if (typeof a === 'object') {
+            let param = Object.keys(a);
+            param.forEach(function (o) {
+                q.dataFD.append(o, a[o]);
+            });
+        }
+        return q;
+    };
+
+    this.then = function (callback, funcerr) {
+        fetch(this.link, {
+            body: this.dataFD,
+            method: "POST", // Gunakan huruf besar untuk metode HTTP
+        })
+            .then(function (r) {
+                return r.json();
+            })
+            .then(callback)
+            .catch(funcerr);
+    };
+
+    return this;
+}
+
+const plyr = {}
+
+plyr.player2 = function(token){
+    let player = document.getElementById("video-container2");
+    let w2 = player.clientWidth;
+    const ply = new ezuikit.EZUIKitPlayer({
+        id: 'video-container2',
+        url: 'ezopen://open.ezviz.com/D78805686/1.live',
+        autoplay: true,
+        accessToken: token,
+        template: 'pcLive',
+        width: w2,
+        height: 400,
+        env: {
+            domain: "https://isgpopen.ezvizlife.com"
+        }
+    });
+    return ply;
+} 
+
+plyr.player = function (token){
+    var player1 = document.getElementById("video-container");
+    var w = player1.clientWidth;
+    const ply = new ezuikit.EZUIKitPlayer({
+        id: 'video-container',
+        url: 'ezopen://open.ezviz.com/D76265573/1.live',
+        autoplay: true,
+        accessToken: token,
+        template: 'pcLive',
+        width: w,
+        height: 400,
+        env: {
+            domain: "https://isgpopen.ezvizlife.com"
+        }
+    });
+} 
+
+const callNewToken = function(id , idplay, appKey, appSecret){
+    new POST('https://open.ezvizlife.com/api/lapp/token/get')
+        .data({
+            appKey: appKey, appSecret: appSecret
+        })
+        .then(function (res) {
+            let data = {
+                token: res.data.accessToken ? res.data.accessToken : null,
+                expired: res.data.expireTime ? res.data.expireTime : null
+            };
+            localStorage.setItem(id, btoa(JSON.stringify(data)));
+            plyr[idplay](data.token);
+        }, function (err) {
+            console.log(err)
+        })
+}
+
+if(!localStorage.getItem('key-client')){
+    callNewToken('key-client', 'player', 'ba8856216e3b4f2681c0c7c706d37df8', '50793f889ca74387bb9028029da6137b');
+} else {
+    let data = JSON.parse(atob(localStorage.getItem('key-client')));
+    if (data.expired < Date.now()){
+        callNewToken('key-client', 'player', 'ba8856216e3b4f2681c0c7c706d37df8', '50793f889ca74387bb9028029da6137b');
+    }else{
+        plyr['player'](data.token)
+        console.log(data.expired)
     }
-});
+};
 
-
-var player2 = new ezuikit.EZUIKitPlayer({
-    id: 'video-container2',
-    url: 'ezopen://open.ezviz.com/D78805686/1.live',
-    autoplay: true,
-    accessToken: "at.8b307h0d69oq3i4s1sfdtssy02d05m2v-6t6tl7w1b7-1msu7lt-nwd7uc2k5",
-    template: 'pcLive',
-    width: w,
-    height: 400,
-    env: {
-        domain: "https://isgpopen.ezvizlife.com"
+if(!localStorage.getItem('key-client2')){
+    callNewToken('key-client2', 'player2', 'cd3807c3ddf64098a4628eb188101b04', '50793f889ca74387bb9028029da6137b');
+} else {
+    let data = JSON.parse(atob(localStorage.getItem('key-client2')));
+    if (data.expired < Date.now()){
+        callNewToken('key-client2', 'player2', 'cd3807c3ddf64098a4628eb188101b04', '50793f889ca74387bb9028029da6137b');
+    }else{
+        plyr['player2'](data.token)
+        console.log(data.expired)
     }
-});
+}
+
+
+
+
+
+
 
 document.getElementById('video-container-play-content').setAttribute("title","Play/Stop");
 document.getElementById('video-container-capturePicture-content').setAttribute("title","Screenshot");
